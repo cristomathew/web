024ff01756869a5e89223afd8d9277a7804151ac
overlord839 <cristomathew7@gmail.com>
2020-02-22 22:46:27 +0530

    Updates

---
+++ precache-manifest.9bba325b37773e3d078d0c73df6b981a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c8235d1a508a0303d12548026ae93e",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "a91de87d1c053b339a85",
+    "url": "/web/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bbd9b50247de0d94ce12",
+    "url": "/web/static/js/2.f3534249.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/web/static/js/2.f3534249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a91de87d1c053b339a85",
+    "url": "/web/static/js/main.d30d5358.chunk.js"
+  },
+  {
+    "revision": "1c971fa92187c11bb958",
+    "url": "/web/static/js/runtime-main.019af177.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web/static/media/logo.5d5d9eef.svg"
+  }
+]);
