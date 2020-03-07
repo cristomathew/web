73b63b9d57ad4504e5dea53ddaf6d9e8bbf580f3
overlord839 <cristomathew7@gmail.com>
2020-03-07 12:03:30 +0530

    rebase

---
+++ build/precache-manifest.a2a039a07fa67643825b29137efcf946.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1205fc91be1eff4ecee54bb8f4cedc51",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "c478f84412a0c86efc3f",
+    "url": "/web/static/css/main.e0b5c80c.chunk.css"
+  },
+  {
+    "revision": "3f64b0ac53bb119b459e",
+    "url": "/web/static/js/2.904ad6d7.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/web/static/js/2.904ad6d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c478f84412a0c86efc3f",
+    "url": "/web/static/js/main.6b6f1f69.chunk.js"
+  },
+  {
+    "revision": "1c971fa92187c11bb958",
+    "url": "/web/static/js/runtime-main.019af177.js"
+  }
+]);
---
+++ build/vanita.js
@@ -0,0 +1,19 @@
+VANTA.DOTS({
+    el: "#root",
+    mouseControls: true,
+    touchControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x2d20ff,
+    color2: 0x1414c5,
+    backgroundColor: 0x0
+  })
+
+VANITA.DOTS("#root");
+
+  $(document).ready(function(){
+    hljs.initHighlightingOnLoad();
+  });
+ 
